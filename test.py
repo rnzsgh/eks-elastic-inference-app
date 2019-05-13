a5dfa4bd2882cd6504204a57058854b0deec124e
Ryan <ryannitz@amazon.com>
2019-05-13 16:29:51 -0400

    Add debug

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ def main():
         print('Please set the environment variables for SQS_TASK_QUEUE and SQS_TASK_COMPLETED_QUEUE')
         sys.exit(1)
 
-    print(task_queue_name)
-    print(task_completed_queue_name)
+    #print(task_queue_name)
+    #print(task_completed_queue_name)
 
     # Get the instance information
     r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
@@ -27,25 +27,34 @@ def main():
     task_queue = boto3.resource('sqs', region_name=region).get_queue_by_name(QueueName=task_queue_name)
     task_completed_queue = boto3.resource('sqs', region_name=region).get_queue_by_name(QueueName=task_completed_queue_name)
 
+    print('Instance initialized: ' + instance_id)
+
     while True:
         for message in task_queue.receive_messages(WaitTimeSeconds=20):
             try:
-                print('message received')
+                print('Message received - instance: ' + instance_id)
                 ec2_client.client.modify_instance_attribute(
                     InstanceId=instance_id,
                     DisableApiTermination={ 'Value': True },
                 )
+                print('Termination protection engaged - instance: ' + instance_id)
+
                 message.change_visibility(VisibilityTimeout=600)
+                print('Message visibility updated - instance: ' + instance_id)
 
                 # Process the message
 
                 task_completed_queue.send_message(MessageBody='completed')
+                print('Task completed msg sent - instance: ' + instance_id)
                 message.delete()
+                print('Message deleted - instance: ' + instance_id)
 
                 ec2_client.client.modify_instance_attribute(
                     InstanceId=instance-id,
                     DisableApiTermination={ 'Value': False },
                 )
+                print('Termination protection disengaged - instance: ' + instance_id)
+
             except:
                 print('Problem processing message')
 
