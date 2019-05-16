2b12ecb9b7f40f404b8a02d7236ecd58fe0879f7
Ryan <ryannitz@amazon.com>
2019-05-15 21:17:14 -0400

    Add tf serving

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,20 @@
 
 TAG=$(git log -1 --pretty=%H)
 
-REPOSITORY=rnzdocker1/eks-elastic-inference-app
+echo EI APP --------------------------------------------------------------------
 
-docker build --tag $REPOSITORY:$TAG .
+APP_REPOSITORY=rnzdocker1/eks-elastic-inference-app
 
-docker push $REPOSITORY:$TAG
+docker build --tag $APP_REPOSITORY:$TAG .
+
+docker push $APP_REPOSITORY:$TAG
+
+echo EI TensorFlow Serving -----------------------------------------------------
+
+SERVING_REPOSITORY=rnzdocker1/eks-elastic-inference-serving
+
+docker build --file Dockerfile_tf_serving --tag $SERVING_REPOSITORY:$TAG .
+
+docker push $SERVING_REPOSITORY:$TAG
+
+
