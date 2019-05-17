2fd531be9cce17268d76779d8fcaa5b27be05853
Ryan <ryannitz@amazon.com>
2019-05-17 12:26:48 -0400

    Hacks

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import logging
 import coco_label_map
 
 ENDPOINT = 'http://localhost:8501/v1/models/default:predict'
-TMP_FILE = './tmp.mov'
+TMP_FILE = '/tmp.mov'
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,8 +21,12 @@ logging.basicConfig(
 log = logging.getLogger()
 
 def get_prediction_from_image_array(image_array):
+    log.info('get_prediction_from_image_array')
+
     payload = {"instances": [image_array.tolist()]}
+    log.info('get_prediction_from_image_array  before endpoint call')
     res = requests.post(ENDPOINT, json=payload)
+    log.info('get_prediction_from_image_array  after endpoint call')
     return res.json()['predictions'][0]
 
 def get_classes_with_scores(predictions):
@@ -32,15 +36,21 @@ def get_classes_with_scores(predictions):
     detection_scores = predictions['detection_scores'][:num_detections]
     return list(zip(detected_classes, detection_scores))
 
-def process_video_from_file(file_path):
+def process_video_from_file(file_path, instance_id):
+
+    log.info('Start processing video - instance: %s', instance_id)
+
     frames = []
     vidcap = cv2.VideoCapture(file_path)
     success, frame = vidcap.read()
     success = True
+
+    log.info('processing video first loop - instance: %s', instance_id)
     while success:
       frames.append(frame)
       success, frame = vidcap.read()
 
+    log.info('processing video second loop - instance: %s', instance_id)
     pred_list = []
     for frame in frames:
         preds = get_prediction_from_image_array(frame)
@@ -101,7 +111,8 @@ def main():
                 s3.download_file(doc['bucket'], doc['object'], TMP_FILE)
                 log.info('File is downloaded - instance: %s', instance_id)
 
-                predictions_for_frames = process_video_from_file(TMP_FILE)
+                log.info('Starting predictions - instance: %s', instance_id)
+                predictions_for_frames = process_video_from_file(TMP_FILE, instance_id)
                 log.info('Predictions completed - instance: %s', instance_id)
 
                 task_completed_queue.send_message(MessageBody=''.join(e for e in predictions_for_frames))
